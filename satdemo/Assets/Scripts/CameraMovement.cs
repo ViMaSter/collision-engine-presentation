1e4dfccd3435eb90058feeef5eda3328656041eb
Vincent Mahnke <accounts@vimaster.de>
2015-05-31 05:40:25 +0200

    ~ Reorganized repo from quadtree-demo to general collision-detection-demo

---
+++ satdemo/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+    public Vector3 Pivot;
+    public Vector3 Offset;
+    public float Distance = 2.0f;
+    public float MouseMoveSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Offset += new Vector3(
+            Input.GetAxisRaw("Mouse Y")*-1,
+            Input.GetAxisRaw("Mouse X")*-1,
+            0.0f
+        );
+
+        transform.position = Pivot;
+        transform.eulerAngles = Offset;
+
+        transform.eulerAngles = new Vector3(
+            transform.eulerAngles.x,
+            transform.eulerAngles.y,
+            0.0f
+        );
+
+        transform.position += transform.TransformDirection(Vector3.forward * Distance);
+
+        transform.LookAt(Pivot);
+	}
+}
---
+++ satdemo/Assets/Scripts/CubeStates.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeStates : MonoBehaviour {
+    public bool currentlyShowCubes = true;
+    public int cubeGroupID = 0;
+    public GameObject[] cubeGroups;
+
+    void ChangeShadows(bool showCubes)
+    {
+        foreach (MeshRenderer renderer in cubeGroups[cubeGroupID].GetComponentsInChildren<MeshRenderer>())
+        {
+            renderer.shadowCastingMode = showCubes ? UnityEngine.Rendering.ShadowCastingMode.On : UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+        }
+
+        currentlyShowCubes = showCubes;
+    }
+
+    void ChangeGroupID(int newID)
+    {
+        cubeGroups[cubeGroupID].SetActive(false);
+        cubeGroupID = newID;
+        cubeGroups[cubeGroupID].SetActive(true);
+    }
+	
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            ChangeGroupID(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ChangeGroupID(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            ChangeShadows(!currentlyShowCubes);
+        }
+	}
+}
